f206f0892b4179d775d9e18cec49f24da012869b
shin <nhathanh024@gmail.com>
2025-05-13 08:18:36 +0700

    update ldp

--- src/wp-content/themes/flatsome-child/assets/js/slider.js
+++ src/wp-content/themes/flatsome-child/assets/js/slider.js
@@ -83,29 +83,8 @@ function testimonialDes() {
 }
 
 jQuery(document).ready(function ($) {
-  initSlider();
-
-  function Marquee(selector, speed) {
-    const parentSelector = document.querySelector(selector);
-    console.log(parentSelector)
-    const clone = parentSelector.innerHTML;
-    const firstElement = parentSelector.children[0];
-    let i = 0;
-    console.log(firstElement);
-    parentSelector.insertAdjacentHTML("beforeend", clone);
-    parentSelector.insertAdjacentHTML("beforeend", clone);
-
-    setInterval(function () {
-      firstElement.style.marginLeft = `-${i}px`;
-      if (i > firstElement.clientWidth) {
-        i = 0;
-      }
-      i = i + speed;
-    }, 0);
-  }
-  // window.addEventListener("load", Marquee(".gallery-customize", 0.));
-
   setTimeout(function () {
+    initSlider();
     marqueeInit({
       uniqueid: "gallery-customize",
       style: {},
