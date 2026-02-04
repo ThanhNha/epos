1b58aaabc7f2b03ba633f6c46220ade60746d6cb
toanpham2111 <toan444666@gmail.com>
2026-02-04 17:46:53 +0700

    udpate mcc

--- src/wp-content/themes/flatsome-child/includes/epos_bluetap.php
+++ src/wp-content/themes/flatsome-child/includes/epos_bluetap.php
@@ -1,10 +1,13 @@
 <?php
 // Check cart item has Bluetap360
-function cart_has_product_bluetap360() {
-    if (WC()->cart->is_empty()) { return false; }
+function cart_has_product_bluetap360()
+{
+    if (WC()->cart->is_empty()) {
+        return false;
+    }
 
     foreach (WC()->cart->get_cart() as $cart_item) {
-        if ((int) $cart_item['product_id'] === 39234) {
+        if ((int) $cart_item['product_id'] === 34592) {  //39234
             return true;
         }
     }
@@ -37,8 +40,12 @@ add_action('woocommerce_checkout_process', function () {
 // Show in order dashboard
 add_action('woocommerce_admin_order_data_after_billing_address', function ($order) {
     $eg = $order->get_meta('order_eg');
+    $mcc = $order->get_meta('_supported_mcc');
     if ($eg) {
         echo '<p><strong>' . __('UEN/Business Registration Number', 'woocommerce') . ':</strong> ' . esc_html($eg) . '</p>';
+    }
+    if ($mcc) {
+        echo '<p><strong>Supported MCC:</strong> ' . esc_html($mcc) . '</p>';
     }
 });
 
@@ -55,10 +62,18 @@ add_action('woocommerce_order_details_after_customer_details', function ($order)
 // Show in mail
 add_filter('woocommerce_email_order_meta_fields', function ($fields, $sent_to_admin, $order) {
     $eg = $order->get_meta('order_eg');
+     $mcc = $order->get_meta('_supported_mcc');
     if ($eg) {
         $fields['order_eg'] = [
             'label' => __('UEN/Business Registration Number', 'woocommerce'),
             'value' => $eg,
+        ];
+    }
+
+    if ($mcc) {
+        $fields['supported_mcc'] = [
+            'label' => 'Supported MCC',
+            'value' => $mcc,
         ];
     }
     return $fields;
@@ -66,7 +81,7 @@ add_filter('woocommerce_email_order_meta_fields', function ($fields, $sent_to_ad
 
 
 
-add_action('woocommerce_checkout_create_order', function($order, $data) {
+add_action('woocommerce_checkout_create_order', function ($order, $data) {
     // Handle custom order eg field
     if (!empty($_POST['order_eg'])) {
         $order->update_meta_data(
@@ -74,4 +89,45 @@ add_action('woocommerce_checkout_create_order', function($order, $data) {
             sanitize_text_field($_POST['order_eg'])
         );
     }
+    if (! empty($_POST['supported_mcc'])) {
+        $order->update_meta_data(
+            '_supported_mcc',
+            sanitize_text_field($_POST['supported_mcc'])
+        );
+    }
 }, 99, 2);
+
+
+add_action('woocommerce_after_checkout_billing_form', function ($checkout) {
+
+    if (! cart_has_product_bluetap360()) {
+        return;
+    }
+
+    $json_path = get_stylesheet_directory() . '/assets//json/mcc-options.json';
+    $options   = json_decode(file_get_contents($json_path), true);
+?>
+
+    <div id="supported-mcc-field">
+        <label for="supported_mcc">Supported MCC <span class="required" aria-hidden="true">*</span></label>
+
+        <select name="supported_mcc" class="woocommerce-select" required>
+            <option value="">-- Select MCC --</option>
+
+            <?php foreach ($options as $option): ?>
+                <option value="<?php echo esc_attr($option['value']); ?>">
+                    <?php echo esc_html($option['label'] . ' - ' . $option['value']); ?>
+                </option>
+            <?php endforeach; ?>
+
+        </select>
+    </div>
+
+<?php
+});
+add_action('woocommerce_checkout_process', function () {
+    if (cart_has_product_bluetap360()  && empty($_POST['supported_mcc'])) {
+        wc_add_notice(__('Please select Supported MCC'), 'error');
+    }
+});
+
